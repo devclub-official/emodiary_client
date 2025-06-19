934ab33231f64eb0a58379cac4310ce87e67b0e6
강노아 <knoa0405@gmail.com>
2025-06-20 00:34:10 +0900

    refactor: Modify getUser function to return structured response and update callback handling for login status

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,10 +1,9 @@
 "use server";
 
 import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import { TypeSearchParams } from "../auth/callback/page";
 
-export async function getUser(params: Awaited<TypeSearchParams>) {
+export const getUser = async (params: Awaited<TypeSearchParams>) => {
   const searchParams = new URLSearchParams();
   Object.entries(params).forEach(([k, v]) => {
     if (Array.isArray(v)) {
@@ -20,27 +19,41 @@ export async function getUser(params: Awaited<TypeSearchParams>) {
     `${process.env.API_BASE_URL}/api/login/google?${searchParams.toString()}`
   );
 
-  console.warn(response);
-
   if (!response.ok) {
     if (response.status === 500) {
-      redirect("/?loginError=serverError");
+      return {
+        status: response.status,
+        message: "서버 오류가 발생했습니다.",
+      };
     }
+
+    if (response.status === 400) {
+      return {
+        status: response.status,
+        message: "잘못된 요청입니다.",
+      };
+    }
+
+    return {
+      status: response.status,
+      message: "알 수 없는 서버 에러가 발생했습니다.",
+    };
   }
 
-  if (response.ok) {
-    const { access_token, refresh_token } = await response.json();
-    const cookieStore = await cookies();
+  const { access_token, refresh_token } = await response.json();
+  const cookieStore = await cookies();
 
-    cookieStore.set("access_token", access_token, {
-      httpOnly: true,
-      path: "/",
-    });
-    cookieStore.set("refresh_token", refresh_token, {
-      httpOnly: true,
-      path: "/",
-    });
+  cookieStore.set("access_token", access_token, {
+    httpOnly: true,
+    path: "/",
+  });
+  cookieStore.set("refresh_token", refresh_token, {
+    httpOnly: true,
+    path: "/",
+  });
 
-    redirect("/dashboard");
-  }
-}
+  return {
+    status: response.status,
+    message: "로그인에 성공했습니다.",
+  };
+};
--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,4 +1,5 @@
 import { getUser } from "@/app/actions";
+import { redirect } from "next/navigation";
 
 export type TypeSearchParams = Promise<{
   [key: string]: string | string[] | undefined;
@@ -10,6 +11,19 @@ export default async function CallbackPage({
   searchParams: TypeSearchParams;
 }) {
   const params = await searchParams;
-  await getUser(params);
+  const response = await getUser(params);
+
+  if (response.status === 200) {
+    redirect("/dashboard");
+  }
+
+  if (response.status === 400) {
+    redirect("/?loginError=serverError");
+  }
+
+  if (response.status === 500) {
+    redirect("/?loginError=serverError");
+  }
+
   return <div>callback page</div>;
 }
